06233e378e8faf63b8b42360c0da6482440ef5f5
lechuck <lechuck@kstec.co.kr>
2015-11-01 22:09:44 +0900

    run deskewEDFilter test using goRoutine

--- src/lec3-ip/deskewEDFilter_test.go
+++ src/lec3-ip/deskewEDFilter_test.go
@@ -3,16 +3,19 @@ import (
 	"testing"
 	"image/color"
 	"fmt"
+	"sync"
 )
 
+type DeskewEDCase struct {
+	option          DeskewEDOption
+	rotation        float32
+	rotatedAngleMin float32
+	rotatedAngleMax float32
+}
+
 func TestDeskewED(t *testing.T) {
-	cases := []struct {
-		option          DeskewEDOption
-		rotation        float32
-		rotatedAngleMin float32
-		rotatedAngleMax float32
-	}{
-		{
+	cases := []DeskewEDCase{
+		DeskewEDCase{
 			DeskewEDOption{
 				maxRotation: 2,
 				incrStep: 0.2,
@@ -21,7 +24,7 @@ func TestDeskewED(t *testing.T) {
 				//debugMode: true,
 			}, -1.4, 1.2, 1.6,
 		},
-		{
+		DeskewEDCase{
 			DeskewEDOption{
 				maxRotation: 2,
 				incrStep: 0.2,
@@ -32,26 +35,38 @@ func TestDeskewED(t *testing.T) {
 		},
 	}
 
-	epsilon := float32(0.00001)
+	wg := sync.WaitGroup{}
 	for idx, c := range cases {
-		// Create Image (800x1200)
-		width, height, margin := 800, 1200, 50
-		bgColor := color.White
-		rectColor := color.Black
-		srcImg := CreateImageWithRect(width, height, margin, margin, width - margin, height - margin, bgColor, rectColor)
+		wg.Add(1)
+		go testCase(t, idx, c, &wg)
+	}
+	wg.Wait()
+}
 
-		// Rotate Image
-		rotatedImg := RotateImage(srcImg, c.rotation, color.White)
+func testCase(t *testing.T, idx int, c DeskewEDCase, wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
 
-		// Run Filter
-		option := c.option
-		result := NewDeskewEDFilter(option).Run(NewFilterSource(rotatedImg, fmt.Sprintf("filename%v", idx)))
-		rotatedAngle := result.(DeskewEDResult).rotatedAngle
+	epsilon := float32(0.00001)
 
-		// Test result image size
-		if rotatedAngle + epsilon < c.rotatedAngleMin || rotatedAngle - epsilon > c.rotatedAngleMax {
-			t.Errorf("[%v] angle mismatch. exepcted=(%v ~ %v), actual=%v",
-				idx, c.rotatedAngleMin, c.rotatedAngleMax, rotatedAngle)
-		}
+	// Create Image (800x1200)
+	width, height, margin := 800, 1200, 50
+	bgColor := color.White
+	rectColor := color.Black
+	srcImg := CreateImageWithRect(width, height, margin, margin, width - margin, height - margin, bgColor, rectColor)
+
+	// Rotate Image
+	rotatedImg := RotateImage(srcImg, c.rotation, color.White)
+
+	// Run Filter
+	option := c.option
+	result := NewDeskewEDFilter(option).Run(NewFilterSource(rotatedImg, fmt.Sprintf("filename%v", idx)))
+	rotatedAngle := result.(DeskewEDResult).rotatedAngle
+
+	// Test result image size
+	if rotatedAngle + epsilon < c.rotatedAngleMin || rotatedAngle - epsilon > c.rotatedAngleMax {
+		t.Errorf("[%v] angle mismatch. exepcted=(%v ~ %v), actual=%v",
+			idx, c.rotatedAngleMin, c.rotatedAngleMax, rotatedAngle)
 	}
 }
